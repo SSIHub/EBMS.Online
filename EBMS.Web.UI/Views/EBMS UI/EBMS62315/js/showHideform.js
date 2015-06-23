19a2445098f83492d18e4504108a1231834907a5
jcevangelista <jcevangelista77@yahoo.com>
2015-06-23 16:10:22 +0800

    EBMS HTML FILES(Employee, CRM, Sales, Purchasing Module)

---
+++ EBMS.Web.UI/Views/EBMS UI/EBMS62315/js/showHideform.js
@@ -0,0 +1,15 @@
+function showHideform()
+{
+	if(document.getElementById('btn').click
+	
+	{
+		document.getElementById('empinfo').style.display='none';
+	
+	}
+	
+	else{
+	document.getElementById('empinfo').style.display='block';
+	
+	}
+
+}
